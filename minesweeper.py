e4bb11f978d60f9e44f245e8bd765c9f99b5117b
Jude-Beaton <jude.a.beaton@gmail.com>
2024-04-17 20:34:52 +0100

    Added comments to code and made bug fixes

--- minesweeper.py
+++ minesweeper.py
@@ -1,19 +1,29 @@
 import pygame
 import numpy as np
 
+# Constants for the board size and mine number
 BOARDWIDTH = 20
 BOARDHEIGHT = 20
+MINES = 20
 
+
+
+# Tile object stores all data about a tile
+# and the funtions they need to perform
 class Tile():
+
+    # Instantiator class
     def __init__(self, x, y):
         self.revealed = False
         self.sprite = "tile"
         self.mine = False
         self.num = 0
         self.flag = False
+        self.pressed = False
         self.x = x
         self.y = y
     
+    # If tile isn't revealed it will place or remove a flag
     def placeFlag(self):
         if(self.flag == True):
             self.flag = False
@@ -22,19 +32,29 @@ class Tile():
             self.flag = True
             self.sprite = "flag"
 
+    # Set tile clicked to true
+    def press(self):
+        self.pressed = True
+        self.sprite = "bg"
+
+    def release(self):
+        self.pressed = False
+        self.sprite = "tile"
+
+    # Set the tile to being a mine
     def makeMine(self):
         self.mine = True
 
+    # Increment the number representing the surrounding mines
     def incrementNum(self):
         self.num += 1
     
-    def changeSprite(self, new):
-        self.sprite = new
-
+    # Draw the tiles current sprite to the screen buffer
     def drawSprite(self, screen):
         img = pygame.image.load("sprites/" + self.sprite + ".png").convert_alpha()
         screen.blit( img, (self.x*32, self.y*32))
 
+    # Change sprite to what is 'under' the tile
     def reveal(self):
         self.revealed = True
         if(self.mine == True):
@@ -46,10 +66,11 @@ class Tile():
 
     
 
-
-def printBoard(screen, board):
+# Create the board
+def generateBoard(screen, board):
     pygame.display.set_caption('Minesweeper')
 
+    # Create a 2D array of tile objects
     for i in range(BOARDHEIGHT):
         row = []
         for j in range(BOARDWIDTH):
@@ -59,19 +80,26 @@ def printBoard(screen, board):
 
     pygame.display.flip()
 
-def updateScreen(screen, board):
+def revealAll(board, screen):
     for x in range(BOARDWIDTH):
         for y in range(BOARDHEIGHT):
+            board[x][y].reveal()
             board[x][y].drawSprite(screen)
-            
-#def revealAll(board):
+        
     
+
+# Generates mines in all spots that are not (startx, starty)
 def generateMines(board, mines, startx, starty):
+
+    # Place mines randomly, if there is already a mine try again until all mines are placed
     while(mines > 0):
         x = np.random.randint(0,BOARDWIDTH)
         y = np.random.randint(0,BOARDHEIGHT)
 
-        if (board[x][y].revealed == False and board[x][y].mine == False and (x != startx or y != starty)):
+        # Check there is no mine, and it is not on the starting point
+        if (board[x][y].mine == False and (x != startx or y != starty)):
+
+            # Increment the num value of all surrounding tiles
             for xs in range(x - 1, x + 2):
                 for ys in range(y - 1, y + 2):
                     if(xs != x or ys != y):
@@ -80,10 +108,9 @@ def generateMines(board, mines, startx, starty):
                     
                     
             board[x][y].makeMine()
-            # print(x, ", ", y)
             mines -= 1
         
-
+# Recursive function that reveals all adjacent empty tiles
 def floodFill(screen, board, x, y):
     if(x < 0 or x >= BOARDWIDTH or y < 0 or y >= BOARDHEIGHT):
         return
@@ -100,18 +127,24 @@ def floodFill(screen, board, x, y):
 
 
     
-
+# Main function
 def main():
     board=[]
     screen = pygame.display.set_mode((BOARDWIDTH*32, BOARDHEIGHT*32))
-    printBoard(screen, board)
+    generateBoard(screen, board)
 
+    # Clock allows the ability to cap fps 
     clock = pygame.time.Clock()
 
     firstMoveMade = False
 
     running = True
     gameOver = False
+    clicked = False
+    prevx = 0
+    prevy = 0
+
+    # Game loop
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -119,31 +152,60 @@ def main():
             if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
                 running = False
             
+            # While player has not hit a mine
             if (gameOver == False):
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     x = pygame.mouse.get_pos()[0]//32
                     y = pygame.mouse.get_pos()[1]//32
-                    if(event.button == 1):     
+                    if(event.button == 1):
+                        clicked = True
+                        if(board[x][y].revealed == False):
+                            board[x][y].press()
+                            board[x][y].drawSprite(screen)
+                    prevx = x
+                    prevy = y
+                
+                    
+                
+                if event.type == pygame.MOUSEMOTION:
+                    if(clicked == True):
+                        x = pygame.mouse.get_pos()[0]//32
+                        y = pygame.mouse.get_pos()[1]//32
+                        if(board[prevx][prevy].revealed == False):
+                            board[prevx][prevy].release()
+                            board[prevx][prevy].drawSprite(screen)
+                        if(board[x][y].revealed == False):
+                            board[x][y].press()
+                            board[x][y].drawSprite(screen)
+                        prevx = x
+                        prevy = y
+
+                    
+                if event.type == pygame.MOUSEBUTTONUP:
+                    x = pygame.mouse.get_pos()[0]//32
+                    y = pygame.mouse.get_pos()[1]//32
+                    if(event.button == 1): 
+                        clicked = False    
                         if (not firstMoveMade): 
-                            generateMines(board, 20, x, y)
+                            generateMines(board, MINES, x, y)
                             firstMoveMade = True
                         if (board[x][y].mine == True):
-                            board[x][y].reveal()
-                            board[x][y].drawSprite(screen)
+                            revealAll(board, screen)
                             gameOver = True
                         else:
                             floodFill(screen, board, x, y)
-                        
                     if(event.button == 3):
                         board[x][y].placeFlag()
-                        board[x][y].drawSprite(screen)            
+                        board[x][y].drawSprite(screen)
+
+                # Print display buffer to display         
                 pygame.display.flip()
-        clock.tick(30)
+        clock.tick(60)
 
                 
 
                 
-
+# 
 if __name__ == "__main__":
     main()
     pygame.quit()
