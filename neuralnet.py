9db0772f71f6303a725795ad165a3eeef8fcfa45
Jude-Beaton <jude.a.beaton@gmail.com>
2024-06-05 18:36:43 +0100

    Library can create a neural network

--- neuralnet.py
+++ neuralnet.py
@@ -1,5 +1,8 @@
+import numpy.matlib
 import pygame
 import numpy
+
+# Need a matrix library
 
 # Neural Net Class
 # - activation function
@@ -13,19 +16,51 @@ import numpy
 class NeuralNet:
 
     #need to establish no. of layers and make an array for each of those, and then weights and bias matrices for each connection between layers
-    def __init__(self, layers, *args):
-        print(layers, len(args))
-     
-    def activation():
+    def __init__(self, *args):
+        self.layers = len(args)
+        self.input = [0] * args[0]
+        self.output = [0] * args[self.layers - 1]
+        self.middleLayers = []
+        self.bias = []
+        self.weights = [] 
+        for i in range(1, self.layers - 1):
+            self.weights.append(numpy.matlib.rand(args[i], args[i - 1]))
+            self.bias.append([1] * args[i])            
+            self.middleLayers.append([0] * args[i])
+        self.bias.append([1] * args[self.layers - 1])
+        self.weights.append(numpy.matlib.rand(args[self.layers - 1], args[self.layers - 2]))
+        
+    
+    # prints the nodes of the network input - middle layers - output
+    def printNetwork(self):
+        print(self.input)
+        print()
+        for i in range(0, self.layers - 2):
+            print(self.middleLayers[i])
+        print()
+        print(self.output)
+        print()
+        print("Biases:")
+        for i in range(0, self.layers - 1):
+            print(self.bias[i])
+        print()
+        print("Weights:")
+        for i in range(0, self.layers - 1):
+            print(self.weights[i])
+        print()
+
+
+    def activation(self):
         # Sigmoid function
         print("Implement me!")
 
-    def forward():
+    def forward(self):
         print("Implement me!")
 
-    def train():
+    def train(self):
         print("Implement me!")
         
+# Can make some more generic training functions here, e.g train epoch, test etc.
 
 def main():
     background_colour = (234, 212, 252)
@@ -38,7 +73,8 @@ def main():
 
     pygame.display.flip()
 
-    nn = NeuralNet(3, 2, 2, 1)
+    nn = NeuralNet(3, 3, 5, 5, 1)
+    nn.printNetwork()
 
     running = True
     while running:
