b61f5d39b692502c1b8428cba3a6af5fabf6e3f4
Jude-Beaton <jude.a.beaton@gmail.com>
2024-06-05 12:17:01 +0100

    Added some skeleton code for nn

--- neuralnet.py
+++ neuralnet.py
@@ -1,4 +1,5 @@
 import pygame
+import numpy
 
 # Neural Net Class
 # - activation function
@@ -8,6 +9,23 @@ import pygame
 
 # MAIN WILL RUN A TEST PROGRAM THAT PASSES THE XOR PROBLEM THROUGH THE NETWORK
 # After this library has been created we need to edit for cnn for the minesweeper application
+
+class NeuralNet:
+
+    #need to establish no. of layers and make an array for each of those, and then weights and bias matrices for each connection between layers
+    def __init__(self, layers, *args):
+        print(layers, len(args))
+     
+    def activation():
+        # Sigmoid function
+        print("Implement me!")
+
+    def forward():
+        print("Implement me!")
+
+    def train():
+        print("Implement me!")
+        
 
 def main():
     background_colour = (234, 212, 252)
@@ -19,6 +37,8 @@ def main():
     screen.fill(background_colour)
 
     pygame.display.flip()
+
+    nn = NeuralNet(3, 2, 2, 1)
 
     running = True
     while running:
